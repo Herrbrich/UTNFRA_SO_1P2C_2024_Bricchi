cc54423cf85114e387b82db041fa04c56bf61fae
herrbrich <lucasbricchi12@gmail.com>
2024-10-06 22:39:03 +0000

    ADD: Elimino archivo erroneo

--- Punto_A.sh
+++
@@ -1,2 +0,0 @@
-!# bin/bash 
-sudo mkdir -p /Examenes-UTN/{alumno1/parcial{1..3},alumno2/parcial{1..3},alumno3/parcial{1..3},profesores}
---
+++ RTA_SCRIPT_Examen_20241003/Punto_C.sh
@@ -0,0 +1,1 @@
+sudo su -c "whoami" p1c2_2024_A1 | sudo tee /Examenes-UTN/alumno1/validar1.txt
